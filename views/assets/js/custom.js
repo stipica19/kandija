e7984e64f94213f7d57588503dd046f4c501ac43
stipica19 <pipiklepic1@gmail.com>
2020-09-03 20:16:41 +0200

    novo22

--- views/assets/js/custom.js
+++ views/assets/js/custom.js
@@ -15,24 +15,6 @@ jQuery(document).ready(function ($) {
 });
 
 $(document).ready(function () {
-  $(".kratki-tekst").each(function (f) {
-    var newstr = $(this).text().substring(35, 280);
-    $(this).text(newstr);
-
-    $(".kratki-tekst").append(".");
-  });
-});
-
-$(document).ready(function () {
-  $(".kratki-tekst2").each(function (f) {
-    var newstr = $(this).text().substring(59, 180);
-    $(this).text(newstr);
-
-    $(".kratki-tekst2").append(".");
-  });
-});
-
-$(document).ready(function () {
   $("img").click(function () {
     console.log("HALo");
     var src = $(this).attr("src");
