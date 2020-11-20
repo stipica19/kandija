7713e1f1bcd379f2c8543f56fc1dd1cc432a30e6
stipica19 <pipiklepic1@gmail.com>
2020-11-20 20:50:23 +0100

    finalv1

--- routes/groblja.js
+++ routes/groblja.js
@@ -15,38 +15,17 @@ isAdmin = (req, res, next) => {
   next();
 };
 //Set Storage Engine
-const storage = multer.diskStorage({
-  destination: "./views/public/",
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 // INIT Upload
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1000000 },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-}).single("images");
 
-function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images Only!");
-  }
-}
+var upload = multer({ storage: storage }).single("file");
 
 router.get("/", (req, res) => {
   Groblje.find({}, (err, groblja) => {
